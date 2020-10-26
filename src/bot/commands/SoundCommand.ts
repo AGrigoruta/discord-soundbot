b9b9548a26db0d10573ef33b8b39f875476d7228
AGrigoruta <alexander.grigoruta@gmail.com>
2020-10-26 20:59:35 +0200

    copy pasted deletion of message after each scenario in the execution of the sound command

--- src/bot/commands/SoundCommand.ts
+++ src/bot/commands/SoundCommand.ts
@@ -16,20 +16,33 @@ export default class SoundCommand implements Command {
   }
 
   public run(message: Message) {
-    message.delete({ timeout: 5000 })
-      .then(data => console.log(data))
-      .catch(console.error);
-    if (!message.member) return;
+    if (!message.member) {
+      message.delete({ timeout: 1000 })
+        .then(data => console.log('Not a member'))
+        .catch(err => console.error(err))
+      return;
+    }
 
     const sound = message.content;
-    if (!existsSound(sound)) return;
+    if (!existsSound(sound)) {
+      message.delete({ timeout: 1000 })
+        .then(data => console.log('No sound'))
+        .catch(err => console.error(err))
+      return;
+    }
 
     const { channel: voiceChannel } = message.member.voice;
     if (!voiceChannel) {
       message.reply(localize.t('helpers.voiceChannelFinder.error'));
+      message.delete({ timeout: 1000 })
+        .then(data => console.log('No voice channel'))
+        .catch(err => console.error(err))
       return;
     }
 
     this.queue.add(new QueueItem(sound, voiceChannel, message));
+    message.delete({ timeout: 1000 })
+      .then(data => console.log('gud'))
+      .catch(err => console.error(err))
   }
 }
